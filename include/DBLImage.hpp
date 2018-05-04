f1c7071d662908aebceed5d328e36692351c0f2a
Nan Zhao <nan.zhao.career@gmail.com>
2018-05-04 11:01:36 -0700

    checkin image I/O codes

--- include/DBLImage.hpp
+++ include/DBLImage.hpp
@@ -1,10 +1,50 @@
-#ifndef INCLUDE_DBLIMAGE_HPP
-#define INCLUDE_DBLIMAGE_HPP
+/*
+* Copyright(c) 2018 DBL
+* Author: Nan Zhao
+* Created on: 05/02/2018
+*/
+
+#ifndef DBLImage_H
+#define DBLImage_H
+
+/*
+#ifdef  DBL_EXPORTING
+#define DBLAPI __declspec(dllexport) 
+#else
+#define DBLAPI __declspec(dllimport) 
+#endif
+*/
+
+#include <memory>
+
+namespace dbl {
 
 #define PI 3.1415926
 
-class DBLImage {
+typedef unsigned char uint8;
 
-};
+//class DBLAPI DBLImage
+class DBLImage
+{
+public:
+	DBLImage(size_t width, size_t height, size_t bytes_per_pixel);
+	DBLImage(const DBLImage& other);
 
-#endif
+	virtual ~DBLImage();
+
+	size_t width() const;
+	size_t height() const;
+	size_t bytesPerPixel() const;
+
+	uint8* image();
+
+private:
+	DBLImage& operator= (const DBLImage& other);
+	size_t width_;
+	size_t height_;
+	size_t bytes_per_pixel_;
+	uint8* image_;
+};  
+
+}  // namespace dbl
+#endif // DBLImage_H
--- src/DBLImage.cpp
+++ src/DBLImage.cpp
@@ -1,2 +1,47 @@
-#include "DBLImage.hpp"
+/*
+ * Copyright(c) 2018 DBL
+ * Author: Nan Zhao
+ * Created on: 05/02/2018
+ */
 
+#define DBL_EXPORTING // ExportDLL
+#include "DBLImage.hpp"
+//#include "talk/base/logging.h"
+
+namespace dbl {
+
+DBLImage::DBLImage(size_t width, size_t height, size_t bytes_per_pixel) :
+    width_(width), height_(height),
+    bytes_per_pixel_(bytes_per_pixel), image_(nullptr) {
+    if (width_ > 0 && height_ > 0) {
+        size_t size = width_ * height * bytes_per_pixel;
+		image_ = new uint8[size];
+    }
+}
+
+DBLImage::DBLImage(const DBLImage& other) {
+	if (this != &other) { // self-check
+		size_t size = other.width_ * other.height_ * other.bytes_per_pixel_;
+		unsigned char* temp = new unsigned char[size];
+		memcpy(temp, other.image_, size);
+		width_ = other.width_;
+		height_ = other.height_;
+		bytes_per_pixel_ = other.bytes_per_pixel_;
+		std::swap(image_, temp);
+		delete [] temp;
+	}
+}
+
+DBLImage::~DBLImage() {
+  delete[] image_;
+}
+
+size_t DBLImage::width() const { return width_;}
+size_t DBLImage::height() const { return height_;}
+size_t DBLImage::bytesPerPixel() const { return bytes_per_pixel_; }
+
+uint8* DBLImage::image() {
+  return image_;
+}
+
+}  // namespace dbl
